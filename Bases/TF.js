df7eb36e83250c92b90cdb1f1e88788e90198bd8
Johny <angel_neet@outlook.com>
2026-04-08 21:56:18 -0600

    Truthy y Falsy

---
+++ Bases/TF.js
@@ -0,0 +1,35 @@
+// Actuadores como false
+false;
+0;
+"" // (string vacío);
+null;
+undefined;
+NaN // (Not a Number);
+
+//Actuadores de true
+true
+1, -1, 3.14 // (cualquier número que no sea 0)
+"hola" // (cualquier string no vacío)
+"0" // (esto es string, no número)
+// []
+{} // (objeto vacío)
+// function() {}
+
+//Ejemplo deuso:
+let nombre = "";
+
+if (nombre) {
+    console.log("Tiene nombre");  // falsy
+} else {
+    console.log("No tiene nombre"); // Truthy
+}
+
+
+{ usuarios.length && <ListaDeUsuarios /> }  // si length es 0, no muestra nada
+
+// Segundo ejemplo de uso:
+if ("0") console.log("1");
+if (0) console.log("2");
+if ([]) console.log("3");
+if ({}) console.log("4");
+if (null) console.log("5");
