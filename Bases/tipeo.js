cce500076b0561238b3b66381319a6e7411b6995
Johny <angel_neet@outlook.com>
2026-04-08 21:23:09 -0600

    Inicio de clases de explicaición de funcionalidad base en JS

---
+++ Bases/tipeo.js
@@ -0,0 +1,25 @@
+// Ejemplo 1
+let numero = 5; // Numero
+numero  = "texto"; // Vaido, ahora es string
+numero = true; //Ahora es boolean
+numero = [1,2,3]; // Arreglo
+numero = {nombre:"juan"}; // Objeto
+
+let caja = 10;
+console.log(typeof caja); //numero
+
+caja = "Dias";
+console.log(typeof caja); //String
+
+caja = true;
+console.log(typeof caja); // boolean
+
+// Ejemplo 2
+let a = 5;
+const b = 10;
+a = 15;   
+b = 20;   
+
+const c = [1, 2, 3];
+c.push(4);  
+c = [5, 6]; 
