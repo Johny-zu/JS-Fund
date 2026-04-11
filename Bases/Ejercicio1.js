9eb5ad2abf27aa6944404eb5f22914a5ffd45b94
Johny <angel_neet@outlook.com>
2026-04-11 15:03:53 -0600

    Ingreso del primer ejercicio

---
+++ Bases/Ejercicio1.js
@@ -0,0 +1,76 @@
+// 1 escribir un comentario en una linea
+console.log("========================================")
+console.log("Inicio del primer ejercicio")
+
+// 2 escribir un comentario en varias lineas
+console.log("========================================")
+for(let i = 0; i < 2; i++){
+    console.log("Bucle de comentarios")
+}
+// 3 Decalara variables primitivas
+let numero = 2;
+let decimal = 2.2;
+let texto = "texto"
+let VF = false;
+let noIdentificada = NaN;
+let sinValor = null;
+
+// 4 imprimir por consola el valor de todas las variables
+console.log("========================================")
+console.log(numero)
+console.log(decimal)
+console.log(texto)
+console.log(VF)
+console.log(noIdentificada)
+console.log(sinValor)
+
+// 5 impirmir el tipo de variable de las variables
+console.log("========================================")
+console.log(typeof numero)
+console.log(typeof decimal)
+console.log(typeof texto)
+console.log(typeof VF)
+console.log(typeof noIdentificada)
+console.log(typeof sinValor)
+
+// 6 modificar valores de variables por otros del mismo tipo
+numero = 4
+decimal = 23.4
+texto = "nuevo texto"
+VF = true;
+
+// 7 modifica valores de variables por otros de diferente tipo
+numero = 4
+decimal = 23.4
+texto = "nuevo texto"
+VF = true;
+noIdentificada = 2;
+
+console.log("========================================")
+console.log(numero)
+console.log(decimal)
+console.log(texto)
+console.log(VF)
+console.log(noIdentificada)
+console.log(sinValor)
+console.log("========================================")
+console.log(typeof numero)
+console.log(typeof decimal)
+console.log(typeof texto)
+console.log(typeof VF)
+console.log(typeof noIdentificada)
+console.log(typeof sinValor)
+
+// 8 Declarar constantes con valores asociados a todos los los tipos de primitivos
+const numero2 = 2;
+const decimal2 = 2.2;
+const texto2 = "texto"
+const VF2 = false;
+const noIdentificada2 = NaN;
+const sinValor2 = null;
+
+
+// 9 modificar los valores de constante
+//numero2 = "texto"
+
+// 10 comenta las lineas lineas que causen problemas
