9508c5b9c72d027481c44c0afe3fb554e116ca94
Johny <angel_neet@outlook.com>
2026-04-11 16:54:39 -0600

    ingreso de clase de operadores en encarpetado bases

---
+++ Bases/Operadores.js
@@ -0,0 +1,32 @@
+let a = 6, b = 3
+
+console.log(a + b)
+console.log(a - b)
+console.log(a / b)
+console.log(a * b)
+console.log(a % b) // modulo
+console.log(a ** b) // exponente
+
+let test = 2;
+console.log(test)
+test += 2;
+console.log(test)
+
+test -= 2
+console.log(test)
+test /= 2
+console.log(test)
+test *= 2
+console.log(test)
+
+// Comparativos
+console.log(a < b)
+console.log(a > b)
+console.log(a <= b)
+console.log(a >= b)
+console.log(a == "6")
+console.log(a === b)
+console.log(a !== b)
+
+console.log(undefined == null)
+console.log(undefined === null)
