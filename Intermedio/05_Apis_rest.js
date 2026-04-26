f11da15fbe4ab19bf00aa98b491b08c47438b188
Johny <angel_neet@outlook.com>
2026-04-25 21:02:59 -0600

    Metodo fetch con Async/Await

--- Intermedio/05_Apis_rest.js
+++ Intermedio/05_Apis_rest.js
@@ -1,5 +1,5 @@
 // Consumo de API
-fetch('https://jsonplaceholder.typicode.com/posts/creoqeuacanoes')
+fetch('https://jsonplaceholder.typicode.com/posts/Aquino')
     .then(response => {
         //transforma la respuesta en JSON
         return response.json()
@@ -13,3 +13,15 @@ fetch('https://jsonplaceholder.typicode.com/posts/creoqeuacanoes')
         console.log("Error", error)
     })
 
+// Async/await
+async function getPosts(){
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+        const data = await response.json()
+        console.log(data)
+    } catch(error){
+        console.log("Error: ", error)
+    }
+}
+
+getPosts()
